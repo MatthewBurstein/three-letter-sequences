44926f0d76db279fc6b256a4419999e74d44d034
Matt Burstein <matthew.burstein@softwire.com>
2018-05-23 16:36:32 +0100

    Refactor: extract fileservice to one line in Main.main()

--- src/main/java/threelettersequences/Main.java
+++ src/main/java/threelettersequences/Main.java
@@ -8,26 +8,28 @@ import java.nio.file.Paths;
 
 import static threelettersequences.SequenceFinder.sequenceFinder;
 import static threelettersequences.SequenceFinder.traFinderRegex;
+import static threelettersequences.TlsLister.buildList;
 
 public class Main {
 
     public static void main(String[] args) {
+        String fileString = "";
+
+        try {
+            fileString = FileService.readFile(Paths.get("C:\\Users\\MJB\\training\\ThreeLetterSequences\\SampleText.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         System.out.println("Using for loop");
 
-        try {
-            String fileString = FileService.readFile(Paths.get("C:\\Users\\MJB\\training\\ThreeLetterSequences\\SampleText.txt"));
-            System.out.println(sequenceFinder(fileString));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        System.out.println(sequenceFinder(fileString));
 
         System.out.println("Using Regex on whole file");
-        try {
-            String fileString = FileService.readFile(Paths.get("C:\\Users\\MJB\\training\\ThreeLetterSequences\\SampleText.txt"));
-            System.out.println(traFinderRegex(fileString));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        System.out.println(traFinderRegex(fileString));
+
+        System.out.println("List of TLS's");
+        System.out.println(buildList(fileString));
 
     }
 
