e9fb792de9f7385c7608d5c97c512b1ff15d38f0
Matt Burstein <matthew.burstein@softwire.com>
2018-05-23 17:01:22 +0100

    feat: implement buildCountMap()

--- src/main/java/threelettersequences/TlsLister.java
+++ src/main/java/threelettersequences/TlsLister.java
@@ -1,7 +1,9 @@
 package threelettersequences;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,4 +20,23 @@ public class TlsLister {
         }
         return output;
     }
+
+    public static Map<String, Integer> buildCountMap(String fileString) {
+        Map<String, Integer> output = new HashMap<>();
+        Pattern p = Pattern.compile("[a-zA-z]{3}");
+        Matcher m = p.matcher(fileString);
+        int idx = 0;
+        while(m.find(idx)) {
+            String lastMatch = m.group();
+            if(output.get(lastMatch) == null){
+                output.put(lastMatch, 1);
+            } else {
+                int prevValue = output.get(lastMatch);
+                output.put(lastMatch, prevValue + 1);
+            }
+            idx = m.start() + 1;
+        }
+        return output;
+    }
+
 }
--- src/test/java/TlsListerTest.java
+++ src/test/java/TlsListerTest.java
@@ -1,11 +1,14 @@
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static threelettersequences.TlsLister.buildCountMap;
 import static threelettersequences.TlsLister.buildList;
 
 public class TlsListerTest {
@@ -25,6 +28,23 @@ public class TlsListerTest {
         expected.add("his");
         expected.add("isd");
         assertEquals(expected, buildList("th. a thi s'ft hisd"));
+    }
+
+    @Test
+    public void returnsCorrectHashMapForSimpleStrings() {
+        Map<String, Integer> expected = new HashMap<>();
+        expected.put("thi", 1);
+        expected.put("his", 2);
+        assertEquals(expected, buildCountMap("this his"));
+    }
+
+    @Test
+    public void returnsCorrectHashMapForNonLetters() {
+        Map<String, Integer> expected = new HashMap<>();
+        expected.put("thi", 1);
+        expected.put("his", 2);
+        expected.put("das", 2);
+        assertEquals(expected, buildCountMap("h das d 'df' das s this his"));
     }
 
 }
